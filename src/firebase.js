7efa36869a7ee717641032d60c322ca1b3448dec
Paul C <paulcarroll92@gmail.com>
2026-07-10 19:52:42 +0100

    moved firebase config details to enviroment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA71Apgydzfdh_jwtavPJMhcqzRACJoYk0",
-  authDomain: "playnext-8d01b.firebaseapp.com",
-  projectId: "playnext-8d01b",
-  storageBucket: "playnext-8d01b.firebasestorage.app",
-  messagingSenderId: "967781242323",
-  appId: "1:967781242323:web:5432bfc0655848a3e58b26"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
